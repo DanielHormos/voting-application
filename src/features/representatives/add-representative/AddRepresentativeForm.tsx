5c9ee37c8cc585ed431fcdf87f3793172bca999c
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-23 19:36:12 +0100

    fixed form page to add representant

--- src/features/representatives/add-representative/AddRepresentativeForm.tsx
+++ src/features/representatives/add-representative/AddRepresentativeForm.tsx
@@ -1,11 +1,34 @@
 export function AddRepresentativeForm() {
   return (
-    <form action="">
-      <label htmlFor="Full Name">Full Name</label>
-      <input type="text" />
-      <label htmlFor="Email">Email</label>
-      <input type="text" />
-      <button type="submit">Add Representative</button>
+    <form className="flex flex-col items-center  p-6  max-w-md">
+      <h2 className="text-2xl font-bold ">Add Representative</h2>
+
+      <label htmlFor="full-name" className="w-full text-left mb-2 ">
+        Full Name
+      </label>
+      <input
+        id="full-name"
+        type="text"
+        placeholder="Enter full name"
+        className="w-full p-2 mb-4 rounded-md border"
+      />
+
+      <label htmlFor="email" className="w-full text-left mb-2  font-medium">
+        Email
+      </label>
+      <input
+        id="email"
+        type="email"
+        placeholder="Enter email"
+        className="w-full p-2 mb-4 rounded-md border  "
+      />
+
+      <button
+        type="submit"
+        className="mt-4 py-2 border border-white rounded-md"
+      >
+        Add Representative
+      </button>
     </form>
   );
 }
