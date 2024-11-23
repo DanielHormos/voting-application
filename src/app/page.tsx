1dcdce5f4c6ca8cc14dea73dd9aa28b9c774f65d
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-23 13:58:23 +0100

    Added front page to first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <h1>Proposal application</h1>
+      <Link href="/welcome-page">Lets get started</Link>
+    </>
+  );
 }
