4e88acf83af6fc1bd412d1dd9249a830b72db100
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-24 12:13:38 +0100

    added type safety for posting representative

---
+++ src/db/types.ts
@@ -0,0 +1,4 @@
+import { representativesTable } from "./schema";
+
+export type RepresentativeInsert = typeof representativesTable.$inferInsert;
+export type RepresentativeSelect = typeof representativesTable.$inferSelect;
