8503f3bf85621126d9d9a3406fb9ecb39e6205fa
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-24 12:09:25 +0100

    added submit form functionality

--- src/features/representatives/AddRepresentativeForm.tsx
+++ src/features/representatives/AddRepresentativeForm.tsx
@@ -1,4 +1,25 @@
+import { useState } from "react";
+import { RepresentativeFormData, representativeSchema } from "./formValidation";
+import { representativeFeature } from "./instance";
+
 export function AddRepresentativeForm() {
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+
+  async function submitForm(event: React.FormEvent) {
+    event.preventDefault();
+
+    const validatedData: RepresentativeFormData = representativeSchema.parse({
+      fullname,
+      email,
+    });
+
+    await representativeFeature.AddRepresentative(validatedData);
+
+    setFullname("");
+    setEmail("");
+  }
+
   return (
     <form className="flex flex-col items-center  p-6  max-w-md">
       <h2 className="text-2xl font-bold ">Add Representative</h2>
@@ -7,8 +28,10 @@ export function AddRepresentativeForm() {
         Full Name
       </label>
       <input
-        id="full-name"
+        id="fullname"
         type="text"
+        value={fullname}
+        onChange={(e) => setFullname(e.target.value)}
         placeholder="Enter full name"
         className="w-full p-2 mb-4 rounded-md border"
       />
@@ -19,6 +42,8 @@ export function AddRepresentativeForm() {
       <input
         id="email"
         type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
         placeholder="Enter email"
         className="w-full p-2 mb-4 rounded-md border  "
       />
@@ -26,6 +51,7 @@ export function AddRepresentativeForm() {
       <button
         type="submit"
         className="mt-4 py-2 border border-white rounded-md"
+        onClick={submitForm}
       >
         Add Representative
       </button>
