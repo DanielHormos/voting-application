b89396df487817d6336f2c76d666b004bc04d297
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-24 12:12:12 +0100

    created service file to to logic of fetching and posting representative

---
+++ src/features/representatives/service.ts
@@ -0,0 +1,24 @@
+import { RepresentativeInsert } from "@/db/types";
+import { Repository } from "./repository";
+
+export function createService(repository: Repository) {
+  return {
+    async AddRepresentative({ fullname, email }: RepresentativeInsert) {
+      const existingRepresentative = await repository.fetchRepresentatives();
+
+      const doesEmailExist = existingRepresentative.some((representative) => {
+        return representative.email === email;
+      });
+
+      if (doesEmailExist) {
+        console.log("Email already exists");
+        throw new Error("Email already exists");
+      }
+      return await repository.addRepresentative({ fullname, email });
+    },
+
+    async fetchRepresentatives() {
+      return await repository.fetchRepresentatives();
+    },
+  };
+}
