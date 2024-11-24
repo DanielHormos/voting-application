008ab29b6999d0e2402f55f48c7a780ac2e6767c
Daniel Hormos <daniel_danne32@hotmail.com>
2024-11-24 12:12:43 +0100

    added repository to fetch and post data to repository

---
+++ src/features/representatives/repository.ts
@@ -0,0 +1,30 @@
+import { db } from "@/db/index";
+import { representativesTable } from "@/db/schema";
+import { RepresentativeInsert } from "@/db/types";
+
+export function createRepository() {
+  return {
+    async fetchRepresentatives() {
+      try {
+        return await db.select().from(representativesTable);
+      } catch (error) {
+        console.error("Failed to fetch representatives:", error);
+        throw new Error("Unable to fetch representatives");
+      }
+    },
+
+    async addRepresentative({ fullname, email }: RepresentativeInsert) {
+      try {
+        return await db.insert(representativesTable).values({
+          fullname,
+          email,
+        });
+      } catch (error) {
+        console.error("Failed to add representative:", error);
+        throw new Error("Unable to add representative");
+      }
+    },
+  };
+}
+
+export type Repository = ReturnType<typeof createRepository>;
