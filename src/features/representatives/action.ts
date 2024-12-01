f63c6cc2a70f94ea93d5027e9af0f77b8f373f3f
Daniel Hormos <daniel_danne32@hotmail.com>
2024-12-01 23:15:56 +0100

    added action for add fetch and voting for representatives

--- src/features/representatives/action.ts
+++ src/features/representatives/action.ts
@@ -1,5 +1,21 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
 import { representativeFeature } from "./instance";
 
-export async function addRepresentative(fullname: string, email: string) {
-  await representativeFeature.addRepresentative({ fullname, email });
+export async function addRepresentative(data: FormData) {
+  const fullname = data.get("fullname") as string;
+  const email = data.get("email") as string;
+  const representative = {
+    fullname,
+    email,
+    public_votes: 0,
+  };
+
+  await representativeFeature.addRepresentative(representative);
+  revalidatePath("/representatives");
+}
+
+export async function fetchRepresentatives() {
+  return await representativeFeature.fetchRepresentatives();
 }
