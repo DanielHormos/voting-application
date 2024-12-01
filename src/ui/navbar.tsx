9ff9c714b244e6a0b3b7c6fbab6afd1522d12c7c
Daniel Hormos <daniel_danne32@hotmail.com>
2024-12-01 23:13:57 +0100

    fixed navbar

--- src/ui/navbar.tsx
+++ src/ui/navbar.tsx
@@ -4,16 +4,17 @@ import Link from "next/link";
 const navItems = [
   { name: "Home", href: "/" },
   { name: "Representatives", href: "/representatives" },
-  { name: "Add Representative", href: "/add-representative" },
 ];
 
 export function Navbar() {
   return (
-    <ul className="flex gap-8 justify-center bg-gray-100 p-4 ">
+    <ul className="menu menu-horizontal bg-base-200 flex justify-center">
       {navItems.map((navItem) => {
         return (
-          <li key={navItem.href} className="">
-            <Link href={navItem.href as Route}>{navItem.name}</Link>
+          <li key={navItem.href}>
+            <Link href={navItem.href as Route} className="text-3xl">
+              {navItem.name}
+            </Link>
           </li>
         );
       })}
