b24890c22f16f14958a24fe211ffa0a5bf940a9e
Daniel Hormos <daniel_danne32@hotmail.com>
2025-01-11 14:57:35 +0100

    fixed typing in creating election

--- src/features/elections/service.ts
+++ src/features/elections/service.ts
@@ -1,12 +1,12 @@
 import { Db } from "@/db";
 import { createRepository } from "./repository";
-import { ElectionInsert } from "./types";
+import { Election } from "./types";
 
 export function createService(db: Db) {
   const repository = createRepository(db);
 
   return {
-    async addElection(election: ElectionInsert) {
+    async addElection(election: Election) {
       return repository.addElection(election);
     },
     async getAllElections() {
