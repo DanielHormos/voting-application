a730695bda13c1a993291af721cb773c445d7676
Daniel Hormos <daniel_danne32@hotmail.com>
2025-01-11 14:31:52 +0100

    added elections component

---
+++ src/features/elections/components/Elections.tsx
@@ -0,0 +1,21 @@
+import { electionFeature } from "../instance";
+import { ElectionCard } from "./ElectionCard";
+
+export async function Elections() {
+  const elections = await electionFeature.getAllElections();
+
+  return (
+    <div>
+      {elections.map((election) => {
+        return (
+          <ElectionCard
+            key={election.id}
+            title={election.title}
+            status={election.status}
+            choices={election.choices}
+          />
+        );
+      })}
+    </div>
+  );
+}
