6c02136195d67f7180348539ce9bcb76414ec27c
Daniel Hormos <daniel_danne32@hotmail.com>
2025-01-11 08:55:58 +0100

    removed files

--- src/features/representatives/feature.ts
+++
@@ -1,9 +0,0 @@
-import { createRepository } from "./repository";
-import { createService } from "./service";
-
-export function createFeature() {
-  const repository = createRepository();
-  const service = createService(repository);
-
-  return service;
-}
