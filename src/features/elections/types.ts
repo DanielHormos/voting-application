4461a7b8ca9ec395f189ba80143a6e9c4c3bccce
Daniel Hormos <daniel_danne32@hotmail.com>
2025-01-11 14:30:51 +0100

    played with election types

--- src/features/elections/types.ts
+++ src/features/elections/types.ts
@@ -4,7 +4,6 @@ export type ElectionInsert = typeof electionsTable.$inferInsert;
 export type ElectionSelect = typeof electionsTable.$inferSelect;
 
 export type Election = {
-  id: string;
   title: string;
   status: string;
   choices: string[];
