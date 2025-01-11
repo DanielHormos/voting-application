8699cdc9c20438d8fdffb5fe80d380d082e053a5
Daniel Hormos <daniel_danne32@hotmail.com>
2025-01-12 00:19:49 +0100

    fixed repository for representatives

--- src/features/representatives/repository.ts
+++ src/features/representatives/repository.ts
@@ -1,36 +1,63 @@
 import { Db } from "@/db";
-import { representativesTable } from "@/features/representatives/schema";
+import { representativesTable, votesTable } from "./schema";
 import { RepresentativeInsert } from "./types";
-import { votesTable } from "./schema";
+import { sql, eq, count } from "drizzle-orm";
 
 export function createRepository(db: Db) {
   return {
     async getAllRepresentatives() {
-      return await db.select().from(representativesTable);
+      const representativesWithVotes = await db
+        .select({
+          id: representativesTable.id,
+          fullname: representativesTable.fullname,
+          email: representativesTable.email,
+          totalVotes: sql`COUNT(${votesTable.id})`.as("votes"),
+        })
+        .from(representativesTable)
+        .leftJoin(
+          votesTable,
+          eq(representativesTable.id, votesTable.representativeId)
+        )
+        .groupBy(representativesTable.id);
+      return representativesWithVotes;
     },
 
-    async addRepresentative({ fullname, email }: RepresentativeInsert) {
-      return await db.insert(representativesTable).values({
-        fullname,
-        email,
-      });
+    async addRepresentative(representative: RepresentativeInsert) {
+      return await db.insert(representativesTable).values(representative);
     },
 
-    async voteRepresentative(representativeId: string, voteId: string) {
-      return await db.insert(votesTable).values({
-        representativeId,
-        voteId,
-      });
+    async addPublicVote(representativeId: string, publicVoterId: string) {
+      await db.insert(votesTable).values({ representativeId, publicVoterId });
     },
 
-    async checkIfVoterVoted(representativeId: string, voterId: string) {
+    async getRepresentativeById(representativeId: string) {
       return await db
+        .select()
+        .from(representativesTable)
+        .where(eq(representativesTable.id, representativeId));
+    },
+
+    async getRepresentativeVotesById(representativeId: string) {
+      const representatives = await db
+        .select({ totalVotes: count() })
+        .from(votesTable)
+        .where(eq(votesTable.representativeId, representativeId));
+
+      return representatives.map((vote) => ({ totalVotes: vote.totalVotes }));
+    },
+
+    async checkIfPublicVoterVoted(
+      representativeId: string,
+      publicVoterId: string
+    ) {
+      const vote = await db
         .select()
         .from(votesTable)
         .where(
           eq(votesTable.representativeId, representativeId) &&
-            eq(votesTable.voteId.equals(voterId))
+            eq(votesTable.publicVoterId, publicVoterId)
         );
+      return vote.length > 0;
     },
   };
 }
