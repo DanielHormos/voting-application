1e0ada888c55fdc6c7dc5bd8797f6e7152f2156d
Daniel Hormos <daniel_danne32@hotmail.com>
2025-01-11 14:30:25 +0100

    fixed table for elections

--- src/features/elections/schema.ts
+++ src/features/elections/schema.ts
@@ -1,10 +1,7 @@
-import { sql } from "drizzle-orm";
 import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
 
-export const electionsTable = pgTable("elections", {
-  id: uuid()
-    .primaryKey()
-    .default(sql`uuid_generate_v4()`),
+export const electionsTable = pgTable("election", {
+  id: uuid().primaryKey(),
   title: varchar().notNull(),
   status: varchar().notNull().default("ongoing"),
   choices: varchar().array().notNull(),
