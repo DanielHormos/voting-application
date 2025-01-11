401529ae82ac65611a8f6d3e92ac42be64aba954
Daniel Hormos <daniel_danne32@hotmail.com>
2025-01-11 14:57:17 +0100

    generating random uuid in schema

--- src/features/elections/schema.ts
+++ src/features/elections/schema.ts
@@ -1,7 +1,10 @@
+import { sql } from "drizzle-orm";
 import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
 
 export const electionsTable = pgTable("election", {
-  id: uuid().primaryKey(),
+  id: uuid()
+    .primaryKey()
+    .default(sql`gen_random_uuid()`),
   title: varchar().notNull(),
   status: varchar().notNull().default("ongoing"),
   choices: varchar().array().notNull(),
