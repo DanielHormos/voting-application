d274508b259ea5695ff3008329eb4e00353fd4e6
Daniel Hormos <daniel_danne32@hotmail.com>
2025-01-11 08:58:19 +0100

    removed try catch from repository

--- src/features/representatives/repository.ts
+++ src/features/representatives/repository.ts
@@ -1,30 +1,18 @@
-import { db } from "@/db/db";
+import { Db } from "@/db";
 import { representativesTable } from "@/features/representatives/schema";
 import { RepresentativeInsert } from "@/db/types";
 
-export function createRepository() {
+export function createRepository(db: Db) {
   return {
-    async fetchRepresentatives() {
-      try {
-        return await db.select().from(representativesTable);
-      } catch (error) {
-        console.error("Failed to fetch representatives:", error);
-        throw new Error("Unable to fetch representatives");
-      }
+    async getAllRepresentatives() {
+      return await db.select().from(representativesTable);
     },
 
     async addRepresentative({ fullname, email }: RepresentativeInsert) {
-      try {
-        return await db.insert(representativesTable).values({
-          fullname,
-          email,
-        });
-      } catch (error) {
-        console.error("Failed to add representative:", error);
-        throw new Error("Unable to add representative");
-      }
+      return await db.insert(representativesTable).values({
+        fullname,
+        email,
+      });
     },
   };
 }
-
-export type Repository = ReturnType<typeof createRepository>;
