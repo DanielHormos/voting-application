5e995d679dd4946633636fac95265c6d8cedb6e5
Daniel Hormos <daniel_danne32@hotmail.com>
2025-01-12 10:20:46 +0100

    added type, and also display votes

--- src/features/representatives/components/RepresentativeCard.tsx
+++ src/features/representatives/components/RepresentativeCard.tsx
@@ -1,23 +1,34 @@
-import { voteRepresentative } from "../action";
-import { Representative } from "../types";
+"use client";
 
-export function RepresentativeCard({ representatives }) {
+import { addPublicVote } from "../action";
+
+type Props = {
+  name: string;
+  email: string;
+  votes: number;
+  representativeId: string;
+};
+
+export function RepresentativeCard({
+  name,
+  email,
+  representativeId,
+  votes,
+}: Props) {
+  const castVote = async () => {
+    await addPublicVote(representativeId);
+  };
+
   return (
     <div className="p-6 max-w-md mx-auto rounded-xl shadow-md space-y-4">
-      {representatives.map((representer: Representative) => (
-        <div
-          key={representer.id}
-          className="card w-full sm:w-auto bg-base-100 shadow-md overflow-hidden"
-        >
-          <div className="card-body">
-            <h2 className="card-title">{representer.fullname}</h2>
-            <p className="text-lg text-base-content">{representer.email}</p>
-            <button onClick={() => voteRepresentative(representatives.id)}>
-              Vote
-            </button>
-          </div>
+      <div className="card w-full sm:w-auto bg-base-100 shadow-md overflow-hidden">
+        <div className="card-body">
+          <h2 className="card-title">{name}</h2>
+          <p className="text-lg text-base-content">{email}</p>
+          <p className="text-lg text-base-content">{votes}</p>
+          <button onClick={castVote}>Vote</button>
         </div>
-      ))}
+      </div>
     </div>
   );
 }
