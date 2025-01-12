206480f7ffd5a6a272cb3fe714e60f07a4f5d28f
Daniel Hormos <daniel_danne32@hotmail.com>
2025-01-12 10:13:47 +0100

    fixed representative instance

--- src/features/representatives/instance.ts
+++ src/features/representatives/instance.ts
@@ -1,4 +1,8 @@
 import { db } from "@/db";
+import { publicVoteInstance } from "../public-voters/instance";
 import { createService } from "./service";
 
-export const representativeFeature = createService(db);
+export const representativeFeature = createService(
+  db,
+  publicVoteInstance.getPublicVoteData
+);
