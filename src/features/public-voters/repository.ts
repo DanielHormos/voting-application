48ab998b7863a5a58acd9aff5015334bcea221e1
Daniel Hormos <daniel_danne32@hotmail.com>
2025-01-12 15:31:55 +0100

    added getpublicvoterdatabyid

--- src/features/public-voters/repository.ts
+++ src/features/public-voters/repository.ts
@@ -1,10 +1,17 @@
 import { Db } from "@/db";
+import { eq } from "drizzle-orm";
 import { publicVotesTable } from "./schema";
 
 export function createPublicVoteRepository(db: Db) {
   return {
     async getPublicVoteData() {
       return db.select().from(publicVotesTable);
+    },
+    async getPublicVoterDataById(id: string) {
+      return db
+        .select({ id: publicVotesTable.id })
+        .from(publicVotesTable)
+        .where(eq(publicVotesTable.id, id));
     },
     async addPublicVoterById(id: string) {
       return db.insert(publicVotesTable).values({ id });
