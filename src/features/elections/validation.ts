80f4b994bf01634b1741002dee70e63ef2b66862
Daniel Hormos <daniel_danne32@hotmail.com>
2025-01-12 15:31:37 +0100

    fixed validation

--- src/features/elections/validation.ts
+++ src/features/elections/validation.ts
@@ -2,5 +2,7 @@ import { z } from "zod";
 
 export const electionSchema = z.object({
   title: z.string().min(3, "Title must be at least 3 characters long"),
-  choices: z.string().min(3, "Choice must be at least 3 characters long"),
+  choices: z.array(
+    z.string().min(2, "Choice must be at least 3 characters long")
+  ),
 });
