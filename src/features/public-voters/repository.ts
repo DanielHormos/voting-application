73e72d04308a084c4bb31c01a33d206434b3a963
Daniel Hormos <daniel_danne32@hotmail.com>
2025-01-12 10:14:56 +0100

    fixed repository for public votes

--- src/features/public-voters/repository.ts
+++ src/features/public-voters/repository.ts
@@ -1,18 +1,13 @@
+import { Db } from "@/db";
+import { publicVotesTable } from "./schema";
+
 export function createPublicVoteRepository(db: Db) {
   return {
     async getPublicVoteData() {
       return db.select().from(publicVotesTable);
     },
-    async getPublicVoteDataById(id: string) {
-      const publicVote = await db
-        .select({ id: publicVotesTable.id })
-        .from(publicVotesTable)
-        .where(eq(publicVotesTable.id, id));
-
-      return publicVote.map((vote) => vote.id);
-    },
     async addPublicVoterById(id: string) {
-      return db.insert(publicVotersTable).values({ id });
+      return db.insert(publicVotesTable).values({ id });
     },
   };
 }
